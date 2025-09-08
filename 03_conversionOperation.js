1b19c96049b8f5f8b27691f67f4f353f7d4efb6d
hrithik979 <munnametha369@gmail.com>
2025-09-08 14:29:16 +0000

    datatypessummary

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -7,3 +7,29 @@ console.log(typeof(score));
 let valueInNumber = Number (score)
 console.log(typeof valueInNumber);
 console.log( valueInNumber);*/
+
+// operation //
+
+let value = 3
+ let negValue = -value
+// console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "hello"
+
+let str2 = " hitesh"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2" );
+console.log(1 + 2 + "2" );
+
+
---
+++ 05_datatypesSummary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String , Number, Boolean, null, undefined, Symbol BigInt
+
+const score = 100
+
+const scoreValue = 100.3
+
+const  isLoggenIn = false
+
+
+const outsideTemp = null
+let userEmail ;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId)
+
+
+
+//const bigNumber = 345678906445356767n
+
+
+//Reference (Non primitive)
+
+// Array, Objects, Functions 
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj={
+    name: "munna",
+    age : 22,
+}
+
+
+const myfunction= function(){
+console.log("Hello World");
+}
+
+console.log(typeof BigNumber);
