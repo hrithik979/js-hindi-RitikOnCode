80c3b2704d3ce479c0962cd430085b3393f1b606
hrithik979 <munnametha369@gmail.com>
2025-09-24 15:34:09 +0000

    json

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename:"js in hindi ",
+    price:"999",
+    courseInstructor:" hitesh"
+}
+
+
+
+const {courseInstructor: instructor}= course
+
+console.log(instructor);
+
+
+// {
+//     name:" hitesh",
+//     coursename: "js in hindi",
+//     price:"free"
+// }
+
+
+
+[
+    {},
+    {},
+    {}
+]
